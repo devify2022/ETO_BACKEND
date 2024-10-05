3f55135e60fe2c2ce9b346b86c1615f5d0f1b528
sayanpal469 <sayanpal469@gmail.com>
2024-10-06 04:03:06 +0530

    rate limit fixed

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 // Other middleware and route setup
-app.use(rateLimitMiddleware);
+// app.use(rateLimitMiddleware);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
@@ -56,5 +56,5 @@ export { app, server };
 // Start the server
 const PORT = 8080;
 server.listen(PORT, () => {
-  console.log(`Socket is running on http://192.168.31.227:${PORT}`);
+  console.log(`Socket is running on http://localhost:${PORT}`);
 });
