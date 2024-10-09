90f997467c05e9faeb4126dccf5377dbe813b6ab
unknown <dsubhojit063@gmail.com>
2024-10-09 22:53:34 +0530

    socjket url changed

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(
 );
 
 // Other middleware and route setup
-app.use(rateLimitMiddleware);
+// app.use(rateLimitMiddleware);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
