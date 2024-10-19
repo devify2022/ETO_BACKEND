d0974dc527697ccb97ed5ed01978c909e8fe7115
unknown <dsubhojit063@gmail.com>
2024-10-19 20:38:54 +0530

    done

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use("/", paymentRouter);
 
 // Home route
 app.get("/", (req, res) => {
-  res.send("Welcome To EASY TOTO (TRIAL) API!");
+  res.send("Welcome To EASY Toto Oerator (TRIAL) API!");
 });
 
 // Error handling middleware
