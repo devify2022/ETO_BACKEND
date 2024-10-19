a7b307b7ffa0941f2d409c0979e84932835d86d1
unknown <dsubhojit063@gmail.com>
2024-10-19 19:45:27 +0530

    done

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use("/", paymentRouter);
 
 // Home route
 app.get("/", (req, res) => {
-  res.send("Welcome to00 EASY (TRIAL) API!");
+  res.send("Welcome to EASY (TRIAL) API!");
 });
 
 // Error handling middleware
